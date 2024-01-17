a62fc857acd2388baaf041546f9c5f1dfe918afc
José Lucas <josluc2016@gmail.com>
2024-01-17 15:36:31 -0300

    feat: config service and env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    autoLoadEntities: true
-  })],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '../.env.development'
+  }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '../.env.development'
+  })]
 })
 export class AppModule {
   constructor(private dataSource: DataSource) { }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,19 +1,23 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Address } from "src/entities/address";
+import { Category } from "src/entities/category";
+import { Product } from "src/entities/product";
 import { Restaurant } from "src/entities/restaurant";
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'test',
-            entities: [Restaurant],
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: 5432,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            entities: [Restaurant, Address, Category, Product],
             synchronize: true,
-        })
+        }), ConfigModule.forRoot()
     ]
 })
 

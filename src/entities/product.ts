2904625fdec420abfd68d4d1778117bb8e1cdaf3
José Lucas <josluc2016@gmail.com>
2024-01-17 15:35:36 -0300

    fix: product entitie

--- src/entities/product.ts
+++ src/entities/product.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Category } from "./category";
 
 @Entity()
@@ -15,5 +15,6 @@ export class Product {
     @Column()
     description: string;
     @OneToOne(type => Category, category_id => category_id.id)
+    @JoinColumn()
     category_id: number;
 }

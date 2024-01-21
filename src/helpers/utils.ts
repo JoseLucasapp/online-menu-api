e0fa7325112e3415c05746f943abf043f482e81e
José Lucas <josluc2016@gmail.com>
2024-01-21 18:15:38 -0300

    feat: return messages

---
+++ src/helpers/utils.ts
@@ -0,0 +1,12 @@
+export enum ReturnMessages {
+    update = 'Updated',
+    delete = 'Deleted',
+    emailAlreadyUsed = 'Email Already Used',
+    wrongPass = 'Wrong password',
+    userNotFound = 'User not found',
+    notFound = 'Not found',
+    invalidToken = 'Invalid token',
+    accessDenied = 'Access denied',
+    success = 'Success',
+    UnexpectedError = 'Unexpected error'
+}
